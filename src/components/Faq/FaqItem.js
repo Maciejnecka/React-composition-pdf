e7c25fc07619edbe6f0f0a84fe4b8f1eb86b1763
Maciej Necka <macieknecka@gmail.com>
2023-11-20 10:41:54 +0100

    59/97 03. Referencje

--- src/components/Faq/FaqItem.js
+++ src/components/Faq/FaqItem.js
@@ -1,17 +1,16 @@
 'use strict';
 import React from 'react';
 
-const FaqItem = (props) => {
-  const { header, content, isOpen } = props;
-
-  const style = { display: isOpen ? 'block' : 'none' };
-  return (
-    <>
-      <header>{header}</header>
-      <article style={style}>{content}</article>
-    </>
-  );
-};
-
-FaqItem.displayName = 'FaqItem';
-export default FaqItem;
+export default class FaqItem extends React.Component {
+  static displayName = 'FaqItem';
+  render() {
+    const { header, content, isOpen } = this.props;
+    const style = { display: isOpen ? 'block' : 'none' };
+    return (
+      <>
+        <header>{header}</header>
+        <article style={style}>{content}</article>
+      </>
+    );
+  }
+}
